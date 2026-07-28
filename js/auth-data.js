90e2e5882fe327f9c4ebe1888fea5cded3dc592f
asociacionichingqigong <asociacionichingqigong@gmail.com>
2026-07-28 08:33:05 +0200

    Update auth-data.js
    se añade Jordi Holgado

--- js/auth-data.js
+++ js/auth-data.js
@@ -20,5 +20,9 @@
 
 const COURSE_STUDENTS = [
   { nombre: "Alumno/a", user: "eltao", pass: "wuwey2026@" },
-   { nombre: "Marta Carbonell", user: "MartaCarbonell", pass: "Loscambios1234"}
+   { nombre: "Marta Carbonell", user: "MartaCarbonell", pass: "Loscambios1234"}, 
+   { nombre: "Jordi Holgado", user: "JordiHolgado", pass: "Loscambios1234"},
+   { nombre: "", user: "", pass: "Loscambios1234"},
+   { nombre: "", user: "", pass: "Loscambios1234"},
+   
 ];
