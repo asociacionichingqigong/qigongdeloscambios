6cfc9c4ba6a9291afa5c352a6b456c7fc00e2f5a
asociacionichingqigong <asociacionichingqigong@gmail.com>
2026-08-07 08:10:08 +0200

    Create auth-data.js
    actializado 07/08/2026

---
+++ js/auth-data.js
@@ -0,0 +1,12 @@
+
+const COURSE_STUDENTS = [
+  { nombre: "Alumno/a", user: "eltao", pass: "wuwey2026@" },
+   { nombre: "Marta Carbonell", user: "MartaCarbonell", pass: "Loscambios1234"}, 
+   { nombre: "Jordi Holgado", user: "JordiHolgado", pass: "Loscambios1234"},
+   { nombre: "Luisa Ojeda", user: "LuisaOjeda", pass: "Loscambios1234"},
+   { nombre: "Jenny de la Rosa", user: "JennydelaRosa", pass: "Loscambios1234"},   
+   { nombre: "Helena Aguilar", user: "HelenaAguilar", pass: "Loscambios1234"},
+   { nombre: "Paola Mariel", user: "PaolaMariel", pass: "Loscambios1234"}, 
+   { nombre: "Lucie Melanie", user: "LucieMelanie", pass: "Loscambios1234"},
+  
+];
